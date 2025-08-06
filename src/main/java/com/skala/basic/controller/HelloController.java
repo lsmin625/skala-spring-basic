b6d8c7f966e0cbbf3ff73b41b13da5d645570d69
이상민(SKCC) <sm.lee1@sk.com>
2025-08-06 10:21:25 +0900

    hello with post to valid

--- src/main/java/com/skala/basic/controller/HelloController.java
+++ src/main/java/com/skala/basic/controller/HelloController.java
@@ -3,17 +3,23 @@ package com.skala.basic.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skala.basic.data.CourseResponse;
+import com.skala.basic.data.HelloRequest;
 import com.skala.basic.data.HelloResponse;
-import com.skala.basic.service.HelloService;
 import com.skala.basic.service.CourseService;
+import com.skala.basic.service.HelloService;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class HelloController {
 
   private final HelloService helloService;
@@ -28,12 +34,20 @@ public class HelloController {
   @GetMapping("/hello")
   public HelloResponse hello(@RequestParam(defaultValue = "SKALA") String name) {
     // 서비스 호출하여 응답 객체 생성
+    log.info("/hello: GET {}", name);
     return helloService.createMessage(name);
+  }
+
+  @PostMapping("/hello")
+  public HelloResponse postHello(@Valid @RequestBody HelloRequest body) {
+    log.info("/hello: POST {}", body.getName());
+    return helloService.createMessage(body.getName());
   }
 
   @PostMapping("/courses/{name}")
   public CourseResponse getClassInfo(@PathVariable String name, @RequestParam List<String> topics) {
     // Service에서 CourseResponse 생성
+    log.info("/courses: {}", name);
     return courseService.createCourse(name, topics);
   }
 }
