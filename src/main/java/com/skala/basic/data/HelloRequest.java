464a9a52a7093dfe5daeecbe3d8d64d04dce31a4
이상민(SKAX) <sm.lee1@sk.com>
2026-02-19 13:19:33 +0900

    코드 제거

--- src/main/java/com/skala/basic/data/HelloRequest.java
+++ src/main/java/com/skala/basic/data/HelloRequest.java
@@ -1,11 +1,5 @@
 package com.skala.basic.data;
 
-import lombok.Data;
-
-@Data
 public class HelloRequest {
-
-    String name;
-    String email;
-    int age;
+    // name, email, age 필드
 }
--- src/main/java/com/skala/basic/data/HelloResponse.java
+++ src/main/java/com/skala/basic/data/HelloResponse.java
@@ -1,11 +1,6 @@
 package com.skala.basic.data;
 
-import lombok.Data;
-
-@Data
 public class HelloResponse {
 
-    // 성공:0, 실패:1
-    int code;
-    String message;
+    // code, message 필드
 }
